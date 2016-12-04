8c3db7be3baf237ffb9fdce9639a8b6f49df47c7
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-12-04 01:03:12 +0100

    [tx-robot] updated from transifex

---
+++ twofactor_privacyidea/l10n/th_TH.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "twofactor_privacyidea",
+    {
+    "Failed to authenticate." : "การตรวจสอบล้มเหลว",
+    "Failed to authenticate. privacyIDEA error." : "การตรวจสอบล้มเหลว พบข้อผิดพลาด privacyIDEA",
+    "Failed to authenticate. Wrong HTTP return code." : "การตรวจสอบล้มเหลว การส่งรหัส HTTP คืนไม่ถูกต้อง",
+    "privacyIDEA 2FA" : "privacyIDEA 2FA",
+    "Open documentation" : "เปิดเอกสาร"
+},
+"nplurals=1; plural=0;");
