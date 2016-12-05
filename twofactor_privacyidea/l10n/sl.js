8fcf221337197b3e55827b29b25bd858282d3669
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-12-05 01:03:12 +0100

    [tx-robot] updated from transifex

---
+++ twofactor_privacyidea/l10n/sl.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "twofactor_privacyidea",
+    {
+    "Failed to authenticate." : "Overitev je spodletela.",
+    "Failed to authenticate. privacyIDEA error." : "Overitev je spodletela; prejeta je napaka privacyIDEA.",
+    "Failed to authenticate. Wrong HTTP return code." : "Overitev je spodletela; prejeta je napačna koda HTTP.",
+    "privacyIDEA 2FA" : "privacyIDEA 2FA",
+    "Open documentation" : "Odpri dokumentacijo"
+},
+"nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);");
