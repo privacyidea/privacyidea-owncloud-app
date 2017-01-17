2f4915974d9ce431ad48a62dfd6088f6846c7b2a
Friedrich Weber <friedrich.weber@netknights.it>
2017-01-17 11:36:44 +0100

    Documentation tweaks in settings-admin.js

--- twofactor_privacyidea/js/settings-admin.js
+++ twofactor_privacyidea/js/settings-admin.js
@@ -19,7 +19,7 @@
 var BASE_URL = '/apps/twofactor_privacyidea/';
 
 $(document).ready(function () {
-
+    /* Use the /getValue API call to retrieve a string value from the app config */
     var getValue = function (key, callback) {
         $.get(OC.generateUrl(BASE_URL + 'getValue'), {key: key}).done(
             function(result) {
@@ -27,6 +27,7 @@ $(document).ready(function () {
             }
         );
     };
+    /* Use the /setValue API call to set a string value in the app config. */
     var setValue = function (key, value) {
         $.post(OC.generateUrl(BASE_URL + 'setValue'), {
             key: key,
@@ -34,6 +35,7 @@ $(document).ready(function () {
         });
     };
 
+    /* privacyIDEA instance URL */
     getValue("url", function (url) {
        $("#piSettings #piurl").val(url);
     });
@@ -45,6 +47,7 @@ $(document).ready(function () {
         setValue("url", value);
     });
 
+    /* "Check SSL" checkbox */
     getValue("checkssl", function (checkssl) {
         $("#piSettings #checkssl").prop('checked', checkssl === "1");
     });
@@ -52,6 +55,7 @@ $(document).ready(function () {
         setValue("checkssl", $(this).is(":checked") ? "1" : "0");
     });
 
+    /* "Bypass Proxy" checkbox */
     getValue("noproxy", function (noproxy) {
         $("#piSettings #noproxy").prop('checked', noproxy === "1");
     });
@@ -59,6 +63,7 @@ $(document).ready(function () {
         setValue("noproxy", $(this).is(":checked") ? "1" : "0");
     });
 
+    /* privacyIDEA realm */
     getValue("realm", function (realm) {
         $("#piSettings #pirealm").val(realm);
     });
@@ -70,6 +75,7 @@ $(document).ready(function () {
         setValue("realm", value);
     });
 
+    /* Enable/Disable challenge triggering */
     var displayServerCredentials = function (show) {
         if(show) {
             $("#piserviceaccount_credentials").show();
@@ -90,22 +96,24 @@ $(document).ready(function () {
         displayServerCredentials(checked);
     });
 
+    /* privacyIDEA service account username */
     getValue("serviceaccount_user", function (user) {
         $("#piSettings #piserviceaccount_user").val(user);
     });
 
     $("#piSettings #piserviceaccount_user").keyup(function () {
-        console.log("pi: Saving Server Admin User");
+        console.log("pi: Saving Service Account User");
         var value = $("#piSettings #piserviceaccount_user").val();
         setValue("serviceaccount_user", value);
     });
 
+    /* privacyIDEA service account password */
     getValue("serviceaccount_password", function (password) {
         $("#piSettings #piserviceaccount_password").val(password);
     });
 
     $("#piSettings #piserviceaccount_password").keyup(function () {
-        console.log("pi: Saving Server Admin Password");
+        console.log("pi: Saving Service Account Password");
         var value = $("#piSettings #piserviceaccount_password").val();
         setValue("serviceaccount_password", value);
     });
