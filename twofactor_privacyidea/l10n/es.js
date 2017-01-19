f7a6208a0b266f0f49e44b388624948f21b34ac0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-19 01:03:12 +0100

    [tx-robot] updated from transifex

--- twofactor_privacyidea/l10n/es.js
+++ twofactor_privacyidea/l10n/es.js
@@ -1,9 +1,16 @@
 OC.L10N.register(
     "twofactor_privacyidea",
     {
+    "Failed to trigger challenges. privacyIDEA error." : "Fallo al realizar retos. Error de privacyIDEA.",
+    "Failed to trigger challenges. Wrong HTTP return code." : "Fallo al realizar retos. Código de retorno HTTP erróneo.",
+    "Failed to trigger challenges." : "Fallo al realizar retos",
     "Failed to authenticate." : "Falló la autenticación.",
     "Failed to authenticate. privacyIDEA error." : "Falló la autenticación. Error de privacyIDEA.",
     "Failed to authenticate. Wrong HTTP return code." : "Falló la autenticación. Código de retorno HTTP erróneo.",
+    "Failed to fetch authentication token. privacyIDEA error." : "Fallo al traer la señal de autenticación. Error de privacyIDEA.",
+    "Failed to fetch authentication token. Wrong HTTP return code." : "Fallo al traer la señal de autenticación. Código de retorno HTTP erróneo.",
+    "Failed to fetch authentication token. Unauthorized." : "Fallo al traer la señal de autenticación. No autorizado.",
+    "Failed to fetch authentication token." : "Fallo al traer la señal de autenticación.",
     "privacyIDEA 2FA" : "privacyIDEA 2FA",
     "Open documentation" : "Abrir Documentación"
 },
--- twofactor_privacyidea/l10n/sq.js
+++ twofactor_privacyidea/l10n/sq.js
@@ -1,9 +1,16 @@
 OC.L10N.register(
     "twofactor_privacyidea",
     {
+    "Failed to trigger challenges. privacyIDEA error." : "Dështoi vënia në punë e <em>challenges</em>. Gabim privacyIDEA.",
+    "Failed to trigger challenges. Wrong HTTP return code." : "Dështoi vënia në punë e <em>challenges</em>. Kod i gabuar përgjigjeje HTTP.",
+    "Failed to trigger challenges." : "Dështoi vënia në punë e <em>challenges</em>.",
     "Failed to authenticate." : "S’u arrit të bëhej mirëfilltësimi.",
-    "Failed to authenticate. privacyIDEA error." : "S’u arri të bëhej mirëfilltësimi. Gabim privacyIDEA.",
+    "Failed to authenticate. privacyIDEA error." : "S’u arrit të bëhej mirëfilltësimi. Gabim privacyIDEA.",
     "Failed to authenticate. Wrong HTTP return code." : "S’u arrit të bëhej mirëfilltësimi. Kod i gabuar përgjigjeje HTTP.",
+    "Failed to fetch authentication token. privacyIDEA error." : "Dështoi prurja e tokenit të mirëfilltësimit. Gabim privacyIDEA.",
+    "Failed to fetch authentication token. Wrong HTTP return code." : "Dështoi prurja e tokenit të mirëfilltësimit. Kod i gabuar përgjigjeje HTTP.",
+    "Failed to fetch authentication token. Unauthorized." : "Dështoi prurja e tokenit të mirëfilltësimit. I paautorizuar.",
+    "Failed to fetch authentication token." : "Dështoi prurja e tokenit të mirëfilltësimit.",
     "privacyIDEA 2FA" : "privacyIDEA 2FA",
     "Open documentation" : "Hap dokumentimin"
 },
