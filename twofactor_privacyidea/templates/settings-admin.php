547ad3c305b0317edeecbbb9fe94506590f422c6
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2017-01-22 23:22:55 +0100

    Fix privacyIDEA headline in the sidebar
    fix closing tags

--- twofactor_privacyidea/templates/settings-admin.php
+++ twofactor_privacyidea/templates/settings-admin.php
@@ -3,17 +3,16 @@ script('twofactor_privacyidea', 'settings-admin');
 ?>
 
 <div class="section" id="privacyIDEA">
-    <h2><?php p($l->t('privacyIDEA 2FA')); ?>
+    <h2><?php p($l->t('privacyIDEA 2FA')); ?></h2>
     <a target="_blank" rel="noreferrer" class="icon-info svg"
-       title="<?php p($l->t('Open documentation'));?>"
-       href="http://privacyidea.readthedocs.io"></a></h2>
-
+       title="<?php p($l->t('Open documentation')); ?>"
+       href="http://privacyidea.readthedocs.io"></a>
     <p>
-        <em>In a second step of authentication the user is asked to provide a one 
-            time password. The users devices are managed in privacyIDEA. The 
+        <em>In a second step of authentication the user is asked to provide a one
+            time password. The users devices are managed in privacyIDEA. The
             authentication request is forwarded to privacyIDEA.
         </em>
-    </p>    
+    </p>
     <div id="piSettings">
         <p>
             <label for="piurl">URL of the privacyIDEA Server</label>
@@ -23,13 +22,13 @@ script('twofactor_privacyidea', 'settings-admin');
                 For compatibility reasons, you may also specify the URL of the /validate/check endpoint.
             </em>
         </p>
-        
+
         <p>
-        <input id="checkssl" type="checkbox" class="checkbox">
-        <label for="checkssl">
-            Verify the SSL certificate. 
-            Do not uncheck this in productive environments!
-        </label>
+            <input id="checkssl" type="checkbox" class="checkbox">
+            <label for="checkssl">
+                Verify the SSL certificate.
+                Do not uncheck this in productive environments!
+            </label>
         </p>
 
         <p>
@@ -39,7 +38,7 @@ script('twofactor_privacyidea', 'settings-admin');
                 requests to privacyIDEA directly.
             </label>
         </p>
-        
+
         <p>
             <label for="pirealm">User Realm in privacyIDEA (other than default)</label>
             <input type="text" id="pirealm" size="40"/>
@@ -51,15 +50,13 @@ script('twofactor_privacyidea', 'settings-admin');
                 Trigger challenges for challenge-response tokens. Check this if you employ, e.g., SMS or E-Mail tokens.
             </label>
             <div id="piserviceaccount_credentials">
-                <p>
-                    <label for="piserviceaccount_user">Username of privacyIDEA service account</label>
-                    <input id="piserviceaccount_user" type="text" size="40">
-                </p>
-                <p>
-                    <label for="piserviceaccount_password">Password of privacyIDEA service account</label>
-                    <input id="piserviceaccount_password" type="password" size="40">
-                </p>
-            </div>
+        <p>
+            <label for="piserviceaccount_user">Username of privacyIDEA service account</label>
+            <input id="piserviceaccount_user" type="text" size="40"/>
+        </p>
+        <p>
+            <label for="piserviceaccount_password">Password of privacyIDEA service account</label>
+            <input id="piserviceaccount_password" type="password" size="40"/>
         </p>
     </div>
 </div>
