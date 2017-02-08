4f27dcd747cca19ca71a7e23e3f6633731278b7e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-02-08 01:03:13 +0100

    [tx-robot] updated from transifex

---
+++ twofactor_privacyidea/l10n/el.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "twofactor_privacyidea",
+    {
+    "Failed to trigger challenges. privacyIDEA error." : "Αποτυχία δημιουργίας προκλήσεων. σφάλμα privacyIDEA",
+    "Failed to trigger challenges. Wrong HTTP return code." : "Αποτυχία δημιουργίας προκλήσεων. Λανθασμένος επιστρεφόμενος κώδικας ΗΤΤΡ",
+    "Failed to trigger challenges." : "Αποτυχία δημιουργίας προκλήσεων.",
+    "Failed to authenticate." : "Απέτυχε τον έλεγχο ταυτοποίησης",
+    "Failed to authenticate. privacyIDEA error." : "Απέτυχε τον έλεγχο πιστοποίησης. σφάλμα privacyIDEA",
+    "Failed to authenticate. Wrong HTTP return code." : "Απέτυχε τον έλεγχο πιστοποίησης. Επιστροφή λανθασμένου κώδικα ΗΤΤΡ",
+    "Failed to fetch authentication token. privacyIDEA error." : "Αποτυχία ανάκτησης ελέγχου πιστοποίησης. σφάλμα  privacyIDEA",
+    "Failed to fetch authentication token. Wrong HTTP return code." : "Απέτυχε η ανάκτηση πιστοποίησης διακριτικού. Λανθασμένη επιστροφή κώδικα ΗΤΤΡ",
+    "Failed to fetch authentication token. Unauthorized." : "Αποτυχία ανάκτησης έλεγχου ταυτότητας. Μη εγκεκριμένη.",
+    "Failed to fetch authentication token." : "Αποτυχία ανάκτησης  έλεγχου ταυτότητας.",
+    "privacyIDEA 2FA" : "privacyIDEA 2FA",
+    "Open documentation" : "Ανοιχτή τεκμηρίωση."
+},
+"nplurals=2; plural=(n != 1);");
