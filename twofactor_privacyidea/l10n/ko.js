187f3e3f110d8165b724f64d2ced25ae568aab47
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-07 01:03:15 +0200

    [tx-robot] updated from transifex

---
+++ twofactor_privacyidea/l10n/ko.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "twofactor_privacyidea",
+    {
+    "Failed to trigger challenges. privacyIDEA error." : "챌린지를 발생시킬 수 없습니다. privacyIDEA 오류입니다.",
+    "Failed to trigger challenges. Wrong HTTP return code." : "챌린지를 발생시킬 수 없습니다. HTTP 응답 코드가 잘못되었습니다.",
+    "Failed to trigger challenges." : "챌린지를 발생시킬 수 없습니다.",
+    "Failed to authenticate." : "인증할 수 없습니다.",
+    "Failed to authenticate. privacyIDEA error." : "인증할 수 없습니다. privacyIDEA 오류입니다.",
+    "Failed to authenticate. Wrong HTTP return code." : "인증할 수 없습니다. HTTP 응답 코드가 잘못되었습니다.",
+    "Failed to fetch authentication token. privacyIDEA error." : "인증 토큰을 가져올 수 없습니다. privacyIDEA 오류입니다.",
+    "Failed to fetch authentication token. Wrong HTTP return code." : "인증 토큰을 가져올 수 없습니다. HTTP 응답 코드가 잘못되었습니다.",
+    "Failed to fetch authentication token. Unauthorized." : "인증 토큰을 가져올 수 없습니다. 인증되지 않았습니다.",
+    "Failed to fetch authentication token." : "인증 토큰을 가져올 수 없습니다.",
+    "privacyIDEA 2FA" : "privacyIDEA 2FA",
+    "Open documentation" : "문서 열기"
+},
+"nplurals=1; plural=0;");
