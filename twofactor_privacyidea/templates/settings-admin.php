f4e326f2fba903db8ce73e6d3130ea6ebe3c2f65
Micha Preußer <mail@mpreusser.de>
2018-04-20 11:33:13 +0200

    Changed layout and strings to get translation.

--- twofactor_privacyidea/templates/settings-admin.php
+++ twofactor_privacyidea/templates/settings-admin.php
@@ -9,83 +9,133 @@ script('twofactor_privacyidea', 'settings-admin');
        title="<?php p($l->t('Open documentation')); ?>"
        href="http://privacyidea.readthedocs.io"></a></h2>
     <span id="pi_settings_msg" class="msg"></span>
+    <hr>
+    <h3>
+        <?php p($l->t('Configuration')); ?>
+    </h3>
     <p>
-        <em>In a second step of authentication the user is asked to provide a one
-            time password. The users devices are managed in privacyIDEA. The
-            authentication request is forwarded to privacyIDEA.
+        <em>
+            <?php p($l->t('
+                In a second step of authentication the user is asked to provide a one
+                time password. The users devices are managed in privacyIDEA. The
+                authentication request is forwarded to privacyIDEA.
+            ')); ?>
         </em>
     </p>
     <div id="piSettings">
         <p>
             <input id="piactive" type="checkbox" class="checkbox">
-            <label for="piactive">Activate two factor authentication with privacyIDEA.</label>
-            <em>Before activating two factor authentication with privacyIDEA, please asure, that the connection to
-                your privacyIDEA-server is configured correctly.</em>
+            <label for="piactive"><?php p($l->t('Activate two factor authentication with privacyIDEA.')); ?></label>
+            <em>
+                <?php p($l->t('
+                    Before activating two factor authentication with privacyIDEA, please asure, that the connection to
+                    your privacyIDEA-server is configured correctly.
+                ')); ?>
+            </em>
         </p>
         <p>
-            <label for="piexcludegroups">Exclude these groups from two factor authentication</label>
+            <label for="piexcludegroups"><?php p($l->t('Exclude these groups from two factor authentication')); ?></label>
             <input type="text" id="piexcludegroups" width="300px"/>
             <em>
-                Users in the following groups will not need to do two factor authentication.
-                Please enter a comma separated list of group names.
+                <?php p($l->t('
+                    Users in the following groups will not need to do two factor authentication.
+                    Please enter a comma separated list of group names.
+                ')); ?>
             </em>
         <p>
-            <label for="piurl">URL of the privacyIDEA Server</label>
+            <label for="piurl"><?php p($l->t('URL of the privacyIDEA Server')); ?></label>
             <input type="text" id="piurl" width="300px"/>
             <em>
-                Please use the base URL of your privacyIDEA instance.
-                For compatibility reasons, you may also specify the URL of the /validate/check endpoint.
+                <?php p($l->t('
+                    Please use the base URL of your privacyIDEA instance.
+                    For compatibility reasons, you may also specify the URL of the /validate/check endpoint.
+                ')); ?>
             </em>
         </p>
 
         <p>
             <input id="checkssl" type="checkbox" class="checkbox">
             <label for="checkssl">
-                Verify the SSL certificate.
-                Do not uncheck this in productive environments!
+                <?php p($l->t('
+                    Verify the SSL certificate.
+                    Do not uncheck this in productive environments!
+                ')); ?>
             </label>
         </p>
 
         <p>
             <input id="noproxy" type="checkbox" class="checkbox">
             <label for="noproxy">
-                Ignore the system wide proxy settings and send authentication
-                requests to privacyIDEA directly.
+                <?php p($l->t('
+                    Ignore the system wide proxy settings and send authentication
+                    requests to privacyIDEA directly.
+                ')); ?>
             </label>
         </p>
 
         <p>
-            <label for="pirealm">User Realm in privacyIDEA (other than default)</label>
+            <label for="pirealm"><?php p($l->t('User Realm in privacyIDEA (other than default)')); ?></label>
             <input type="text" id="pirealm" size="40"/>
         </p>
-
+        <hr>
+        <h3>
+            <?php p($l->t('Test')); ?>
+        </h3>
         <p>
-            Test Authentication by supplying username and password that are checked against privacyIDEA:
-	    <label for="pitestauthentication_user">User</label>
-            <input type="text" id="pitestauthentication_user" size="40" />
-            <label for="pitestauthentication_password">Password</label>
-            <input type="password" id="pitestauthentication_password" size="40" />
-            <input id="pitestauthentication" type="button" value="Test" />
+            <?php p($l->t('Test Authentication by supplying username and password that are checked against privacyIDEA:')); ?>
+            <table>
+            <tr>
+                <td>
+                    <label for="pitestauthentication_user"><?php p($l->t('User')); ?></label> &nbsp;
+                </td>
+                <td>
+                    <input type="text" id="pitestauthentication_user" size="40" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="pitestauthentication_password"><?php p($l->t('Password')); ?></label> &nbsp;
+                </td>
+                <td>
+                    <input type="password" id="pitestauthentication_password" size="40" />
+                </td>
+            </tr>
+            </table>
+            <input id="pitestauthentication" type="button" value="<?php p($l->t('Test')); ?>" />
             <span id="pitestauthentication_msg" class="msg"></span>
         </p>
-
+        <hr>
+        <h3>
+            <?php p($l->t('Challenge Response')); ?>
+        </h3>
         <p>
             <input id="triggerchallenges" type="checkbox" class="checkbox">
             <label for="triggerchallenges">
-                Trigger challenges for challenge-response tokens. Check this if you employ, e.g., SMS or E-Mail tokens.
+                <?php p($l->t('Trigger challenges for challenge-response tokens. Check this if you employ, e.g., SMS or E-Mail tokens.')); ?>
             </label>
-            <div id="piserviceaccount_credentials">
-        <p>
-            <label for="piserviceaccount_user">Username of privacyIDEA service account</label>
-            <input id="piserviceaccount_user" type="text" size="40"/>
         </p>
-        <p>
-            <label for="piserviceaccount_password">Password of privacyIDEA service account</label>
-            <input id="piserviceaccount_password" type="password" size="40" autocomplete="new-password" />
-        </p>
-        <p>
-            <input id="piserviceaccount_test" type="button" value="Check Credentials" />
+        <div id="piserviceaccount_credentials">
+
+            <table>
+                <tr>
+                    <td>
+                        <label for="piserviceaccount_user"><?php p($l->t('Username of privacyIDEA service account')); ?></label> &nbsp;
+                    </td>
+                    <td>
+                        <input id="piserviceaccount_user" type="text" size="40"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="piserviceaccount_password"><?php p($l->t('Password of privacyIDEA service account')); ?></label> &nbsp;
+                    </td>
+                    <td>
+                        <input id="piserviceaccount_password" type="password" size="40" autocomplete="new-password" />
+                    </td>
+                </tr>
+            </table>
+            <input id="piserviceaccount_test" type="button" value="<?php p($l->t('Check Credentials')); ?>" />
             <span id="piserviceaccount_msg" class="msg"></span>
-        </p>
+        </div>
     </div>
 </div>
