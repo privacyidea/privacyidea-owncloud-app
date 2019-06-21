579625dddb65068aba9851313324f0879f832506
tamaro-skaljic <tamaro.skaljic@netknights.it>
2019-06-21 11:13:16 +0200

    Don't send form data during the token/challenges/ request

--- twofactor_privacyidea/js/push.js
+++ twofactor_privacyidea/js/push.js
@@ -3,7 +3,6 @@ window.onload=function(){
         $.ajax({
             url:location.href,
             type:'post',
-            data: $('#piLoginForm').serialize(),
             success:window.onload=function(result){
                 if(result.search("<span class=\"warning\">") === -1){
                     location.reload()
