4032f72766e9be7d9fc0381e59db5215cb647415
Nils Behlen <29949516+nilsbehlen@users.noreply.github.com>
2021-12-17 12:15:33 +0100

    Delete sync.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "scp files to owncloud - server 10.0.5.11 "
-scp -r * root@10.0.5.11:/var/www/owncloud/apps-external/twofactor_privacyidea
-
-#scp -r * root@10.0.5.81:/usr/share/simplesamlphp/modules/privacyidea
-# scp -r * root@10.0.5.21:/var/simplesamlphp/modules/privacyidea
