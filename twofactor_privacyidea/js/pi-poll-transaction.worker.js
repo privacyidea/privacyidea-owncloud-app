402696d5773f4225e4ea89c362e395ac03255317
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-12-14 14:38:17 +0100

    poll in browser

--- twofactor_privacyidea/js/pi-poll-transaction.worker.js
+++ twofactor_privacyidea/js/pi-poll-transaction.worker.js
@@ -1,3 +1,21 @@
+/**
+ * @author Lukas Matusiewicz <lukas.matusiewicz@netknights.it>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is a free software: You can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
 let url;
 let params;
 self.addEventListener('message', function (e)
@@ -9,7 +27,7 @@ self.addEventListener('message', function (e)
         case 'url':
             url = data.msg + "/validate/polltransaction";
             break;
-        case 'transactionID':
+        case 'transactionId':
             params = "transaction_id=" + data.msg;
             break;
         case 'start':
